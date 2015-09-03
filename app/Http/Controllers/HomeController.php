b8ce142f943d08050421c674fd2ef10bc35275ca
lee <ligangmingx@gmail.com>
2015-09-03 22:36:46 +0800

    update composer.lock

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+
+    /**
+     * Hello
+     */
+    public function index()
+    {
+        echo 'Hello lee';
+    }
+
+}
